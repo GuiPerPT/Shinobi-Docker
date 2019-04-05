b24546b559fdbf5a37d4ddc4a3dd50e93a549bd1
Ulisses1478 <silvamunizulisses@hotmail.com>
2019-04-05 10:30:38 -0300

    Start.sh added if

--- start.sh
+++ start.sh
@@ -3,5 +3,12 @@ if [ -f "$file" ]
 then
 	echo "$file found."
 else
-git init && git remote add origin https://gitlab.com/Shinobi-Systems/Shinobi.git && git fetch && git checkout -t origin/master && chmod +x INSTALL/start.sh  && chmod +x INSTALL/ubuntu.sh && INSTALL/ubuntu.sh && cp /installation/conf.json /shinobi
+	echo $(pwd)
+if [ -f "/shinobi/.git" ]
+then
+cd /shinobi && git fetch && git checkout -t origin/master && chmod +x INSTALL/start.sh && chmod +x INSTALL/ubuntu.sh && INSTALL/ubuntu.sh && cp /installation/conf.json /shinobi
+else
+cd /shinobi && git init && git remote add origin https://gitlab.com/Shinobi-Systems/Shinobi.git && git fetch && git checkout -t origin/master && chmod +x INSTALL/start.sh && chmod +x INSTALL/ubuntu.sh && INSTALL/ubuntu.sh && cp /installation/conf.json /shinobi
 fi
+fi
+
